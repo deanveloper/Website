e0c03181d62fd8533e4b7c4462ad8f2021ad6142
Dean B <deanvelopermn@gmail.com>
2016-09-02 08:11:46 -0400

    Added a Base64 converter to my website

---
+++ base64/script.js
@@ -0,0 +1,34 @@
+var text;
+var base64;
+window.onload = function () {
+    text = document.getElementById("text");
+    base64 = document.getElementById("base64");
+};
+
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+}
+
+function b64DecodeUnicode(str) {
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
+function textEventHandler() {
+    try {
+        base64.value = b64EncodeUnicode(text.value);
+    } catch (e) {
+        base64.value = "invalid text!"
+    }
+}
+
+function base64EventHandler() {
+    try {
+        text.value = b64DecodeUnicode(base64.value);
+    } catch (e) {
+        text.value = "invalid base64!"
+    }
+}
