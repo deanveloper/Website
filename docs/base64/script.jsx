2a0420166ce337371f5be06c91aec447c3b96570
Dean B <dean@deanveloper.com>
2016-11-07 13:40:55 -0500

    More bug fixes

--- docs/base64/script.jsx
+++ docs/base64/script.jsx
@@ -4,7 +4,14 @@ var base64 = document.getElementById("base64");
 window.addEventListener("load", () => {
     text = document.getElementById("text");
     base64 = document.getElementById("base64");
+    text.addEventListener("keydown", textEventHandler);
+    text.addEventListener("keyup", textEventHandler);
+    text.addEventListener("input", textEventHandler);
+    base64.addEventListener("keydown", base64EventHandler);
+    base64.addEventListener("keyup", base64EventHandler);
+    base64.addEventListener("input", base64EventHandler);
 });
+
 
 function b64EncodeUnicode(str) {
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
