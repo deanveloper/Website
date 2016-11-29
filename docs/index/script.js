603049b93182eaea0d574d7adf1963faef8664c2
Dean Bassett <deanvelopermn@gmail.com>
2016-11-29 02:15:32 -0500

    Now has formatting

--- docs/index/script.js
+++ docs/index/script.js
@@ -4,12 +4,12 @@ const $ = require("jquery");
 const icons = {};
 
 $(document).ready(() => {
-    icons.github = new Icon("code");
-    icons.donate = new Icon("dollar");
-    icons.twitter = new Icon("twitter");
-    icons.contact = new Icon("address-card-o");
-    icons.blog = new Icon("file-text-o");
-    icons.tools = new Icon("wrench");
+    icons.github = new Icon("github-alt", 1);
+    icons.donate = new Icon("dollar", 1);
+    icons.twitter = new Icon("twitter", 1);
+    icons.contact = new Icon("address-card-o", 2);
+    icons.blog = new Icon("file-text-o", 2);
+    icons.tools = new Icon("wrench", 2);
 });
 
 function animateHover(icon) {
@@ -17,8 +17,8 @@ function animateHover(icon) {
 }
 
 class Icon {
-    constructor(type) {
-        $("#items").append(
+    constructor(type, list) {
+        $("#items" + list).append(
             $("<li>").append(
                 $("<div>").attr("id", type).attr("class", "icon").append(
                     $("<i>").attr("class", "fa fa-2x fa-" + type))));
