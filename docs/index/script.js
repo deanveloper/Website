9f152d5f256d5e99e4ab3936192d09a5383a5e56
Dean Bassett <deanvelopermn@gmail.com>
2016-11-29 13:51:12 -0500

    Now we have a working website!

--- docs/index/script.js
+++ docs/index/script.js
@@ -1,15 +1,14 @@
-const anime = require("animejs");
 const $ = require("jquery");
 
 const icons = {};
 
 $(document).ready(() => {
-    icons.github = new Icon("github-alt", 1);
-    icons.donate = new Icon("dollar", 1);
-    icons.twitter = new Icon("twitter", 1);
-    icons.contact = new Icon("address-card-o", 2);
-    icons.blog = new Icon("file-text-o", 2);
-    icons.tools = new Icon("wrench", 2);
+    icons.github = new Icon("github-alt", 1, "https://github.com/Deanveloper", "GitHub");
+    icons.twitter = new Icon("twitter", 1, "https://twitter.com/Deanveloper", "Twitter");
+    icons.donate = new Icon("dollar", 1, "https://paypal.me/Dean98", "Donate");
+    icons.email = new Icon("address-card-o", 2, "mailto:dean@deanveloper.com", "Email");
+    icons.blog = new Icon("file-text-o", 2, "https://blog.deanveloper.com", "Blog [Coming Soon]");
+    icons.tools = new Icon("wrench", 2, "https://www.deanveloper.com/tools", "Tools [Coming Soon]");
 });
 
 function animateHover(icon) {
@@ -17,15 +16,21 @@ function animateHover(icon) {
 }
 
 class Icon {
-    constructor(type, list) {
-        $("#items" + list).append(
-            $("<li>").append(
-                $("<div>").attr("id", type).attr("class", "icon").append(
-                    $("<i>").attr("class", "fa fa-2x fa-" + type))));
-        this.elem = $("#" + type);
-    }
+    constructor(type, list, link, tooltip) {
+        $("#items" + list)
+            .append($("<li>")
+                .append($("<div>").attr("id", type).attr("class", "icon")
+                    .append($("<i>").attr("class", "fa fa-2x fa-" + type)))
+                .append($("<span>").attr("class", "tooltip")
+                    .append(tooltip))
+            );
 
-    onClick() {
+        this.$elem = $("#" + type);
 
+        this.$elem.mouseup(() => {
+            if (!tooltip.endsWith("[Coming Soon]")) {
+                window.location.href = link
+            }
+        });
     }
 }
