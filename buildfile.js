a28f65c193d8d3539b987cc9927ab10d187a32b1
Dean Bassett <deanvelopermn@gmail.com>
2017-01-13 03:19:59 -0500

    renamed to backend

--- buildfile.js
+++ buildfile.js
@@ -2,29 +2,30 @@
  * Created by Dean on 11/4/2016.
  */
 const fs = require("fs");
+const async = require("async");
 const browserify = require("browserify");
 const path = require("path");
 const cheerio = require("cheerio");
 
-const ourFile = process.argv[2];
-if (!ourFile) {
+const ourFilePath = process.argv[2];
+if (!ourFilePath) {
     throw new Error("File must be an argument!");
 }
 
-fs.readFile(ourFile, function (err, fileData) {
+fs.readFile(ourFilePath, function (err, fileData) {
     if (err) {
         throw err;
     }
 
     // If our file is an HTML file, parse the meta file and add onto our html file!
-    if (path.extname(ourFile) === ".html") {
+    if (path.extname(ourFilePath) === ".html") {
         let $ = cheerio.load(fileData);
         let newFile = fileData;
-        const metaFile = path.join(path.dirname(ourFile), "meta.json");
-        const scriptBundle = path.join(path.dirname(ourFile), "bundle.min.js");
+        const metaFile = path.join(path.dirname(ourFilePath), "meta.json");
+        const scriptBundle = path.join(path.dirname(ourFilePath), "bundle.min.js");
         const head = $("head");
 
-        if(!$("html")) {
+        if (!$("html")) {
             return;
         }
 
@@ -34,64 +35,96 @@ fs.readFile(ourFile, function (err, fileData) {
             newFile = $.html();
         }
 
-        if (fs.existsSync(metaFile)) {
-            fs.readFile(metaFile, function (err, metadata) {
-                if (err) {
-                    throw err;
-                }
-
-                const meta = JSON.parse(metadata);
-
-                // define a function to add tags to head
-                function add(type, _toAdd, def) {
-                    let elem = head.prepend("<" + type + ">");
-                    const toAdd = _toAdd ? _toAdd : def;
-                    for (const index in toAdd) {
-                        if (toAdd.hasOwnProperty(index)) {
-                            elem = elem.attr(index, toAdd[index]);
+        async.parallel([
+            function scanMetaFile() {
+                if (fs.existsSync(metaFile)) {
+                    fs.readFile(metaFile, function (err, metadata) {
+                        if (err) {
+                            throw err;
                         }
-                    }
-                    newFile = $.html();
-                }
 
-                // function to add links to <head>
-                const addLink = (toAdd, rel, href, size) => add("link", toAdd, {rel: rel, href: href, size: size});
-                // function to add meta to <head>
-                const addMeta = (toAdd, name, content) => add("link", toAdd, {name: name, content: content});
+                        const meta = JSON.parse(metadata);
 
-                if (!meta.favicon) {
-                    meta.favicon = {};
-                }
-                if (!meta.card) {
-                    meta.card = {};
-                }
+                        // define a function to add tags to head
+                        function add(type, _toAdd, def) {
+                            let elem = head.prepend("<" + type + ">");
+                            const toAdd = _toAdd ? _toAdd : def;
+                            for (const index in toAdd) {
+                                if (toAdd.hasOwnProperty(index)) {
+                                    elem = elem.attr(index, toAdd[index]);
+                                }
+                            }
+                            newFile = $.html();
+                        }
 
-                if (!meta.favicon.noFavicon) {
-                    addLink(meta.favicon.apple, "apple-touch-icon", "/favicon/apple-touch-icon.png", "180x180");
-                    addLink(meta.favicon.favlarge, "icon", "/favicon/favicon-32x32.png", "32x32");
-                    addLink(meta.favicon.favsmall, "icon", "/favicon/favicon-16x16.png", "16x16");
-                    addLink(meta.favicon.manifest, "manifest", "/favicon/manifest.json");
-                    addLink(meta.favicon.ico, {rel: "shortcut icon", href: "/favicon/favicon.ico"});
-                    addMeta(meta.favicon.ms, "msapplication-config", "/favicon/browserconfig.xml");
-                    addMeta(meta.favicon.theme, "theme-color", "#ffffff");
-                }
+                        // function to add links to <head>
+                        const addLink = (toAdd, rel, href, size) => add("link", toAdd, {
+                            rel: rel,
+                            href: href,
+                            size: size
+                        });
+                        // function to add meta to <head>
+                        const addMeta = (toAdd, name, content) => add("link", toAdd, {name: name, content: content});
 
-                if (!meta.card.noCard) {
-                    addMeta(meta.card.twitterCard, "twitter:card", "summary");
-                    addMeta(meta.card.twitterUser, "twitter:site", "@Deanveloper");
-                    addMeta(meta.card.ogType, "og:type", "website");
-                    addMeta(meta.card.title, "og:title", "Dean Bassett's Website");
-                    addMeta(meta.card.title, "twitter:title", "Dean Bassett's Website");
-                    addMeta(meta.card.description, "og:description", "A place for my little creations.");
-                    addMeta(meta.card.description, "twitter:description", "A place for my little creations.");
-                    addMeta(meta.card.image, "og:image", "https://www.deanveloper.com/favicon/android-chrome-512x512.png");
-                    addMeta(meta.card.image, "twitter:image", "https://www.deanveloper.com/favicon/android-chrome-512x512.png");
-                    addMeta(meta.card.twitterImageAlt, "twitter:image:alt", "Dean Bassett's Website");
+                        if (!meta.favicon) {
+                            meta.favicon = {};
+                        }
+                        if (!meta.card) {
+                            meta.card = {};
+                        }
+
+                        if (!meta.favicon.noFavicon) {
+                            addLink(meta.favicon.apple, "apple-touch-icon", "/favicon/apple-touch-icon.png", "180x180");
+                            addLink(meta.favicon.favlarge, "icon", "/favicon/favicon-32x32.png", "32x32");
+                            addLink(meta.favicon.favsmall, "icon", "/favicon/favicon-16x16.png", "16x16");
+                            addLink(meta.favicon.manifest, "manifest", "/favicon/manifest.json");
+                            addLink(meta.favicon.ico, {rel: "shortcut icon", href: "/favicon/favicon.ico"});
+                            addMeta(meta.favicon.ms, "msapplication-config", "/favicon/browserconfig.xml");
+                            addMeta(meta.favicon.theme, "theme-color", "#ffffff");
+                        }
+
+                        if (!meta.card.noCard) {
+                            addMeta(meta.card.twitterCard, "twitter:card", "summary");
+                            addMeta(meta.card.twitterUser, "twitter:site", "@Deanveloper");
+                            addMeta(meta.card.ogType, "og:type", "website");
+                            addMeta(meta.card.title, "og:title", "Dean Bassett's Website");
+                            addMeta(meta.card.title, "twitter:title", "Dean Bassett's Website");
+                            addMeta(meta.card.description, "og:description", "A place for my little creations.");
+                            addMeta(meta.card.description, "twitter:description", "A place for my little creations.");
+                            addMeta(meta.card.image, "og:image", "https://www.deanveloper.com/favicon/android-chrome-512x512.png");
+                            addMeta(meta.card.image, "twitter:image", "https://www.deanveloper.com/favicon/android-chrome-512x512.png");
+                            addMeta(meta.card.twitterImageAlt, "twitter:image:alt", "Dean Bassett's Website");
+                        }
+                    })
                 }
+            }
+        ], function onComplete() {
+            fs.writeFile(getOutputName(), newFile, (err) => {
+                throw err;
             });
-        }
+        })
+    }
+
+    if (path.extname(ourFilePath) === ".js" && path.basename(ourFilePath) !== "bundle.min.js") {
+        browserify(ourFilePath)
+            .transform("babelify", {presets: ["es2015"]})
+            .transform("uglifyify", {global: true})
+            .bundle()
+            .pipe(fs.createWriteStream(getOutputName()))
     }
 });
+
+function getOutputName() {
+    const split = ourFilePath.split(path.sep);
+    for (let i = split.length - 1; i >= 0; i--) {
+        if (split[i] === "backend") {
+            split[i] = "docs";
+            break;
+        }
+    }
+
+    return path.join(...split);
+}
 
 // Walk through all files, look for .jsx files and compile them into minified ES5
 function walk(p) {
@@ -115,14 +148,10 @@ function walk(p) {
             .filter((f) => fs.statSync(f).isFile())
             .forEach((f) => {
                 if (f.endsWith(".js") && !f.endsWith(".min.js")) {
-                    browserify(f)
-                        .transform("babelify", {presets: ["es2015"]})
-                        .transform("uglifyify", {global: true})
-                        .bundle()
-                        .pipe(fs.createWriteStream(path.join(path.dirname(f), "bundle.min.js")))
+
                 }
             });
     });
 }
 
-walk(__dirname + "/site/");
+walk(__dirname + "/backend/");
