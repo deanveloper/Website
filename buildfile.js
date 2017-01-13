163a01cb07751a1560c6e2a27df160a622d3b365
Dean Bassett <deanvelopermn@gmail.com>
2017-01-13 02:59:39 -0500

    better

--- buildfile.js
+++ buildfile.js
@@ -18,64 +18,78 @@ fs.readFile(ourFile, function (err, fileData) {
 
     // If our file is an HTML file, parse the meta file and add onto our html file!
     if (path.extname(ourFile) === ".html") {
+        let $ = cheerio.load(fileData);
+        let newFile = fileData;
         const metaFile = path.join(path.dirname(ourFile), "meta.json");
+        const scriptBundle = path.join(path.dirname(ourFile), "bundle.min.js");
+        const head = $("head");
 
-        fs.readFile(metaFile, function (err, metadata) {
-            if (err) {
-                throw err;
-            }
+        if(!$("html")) {
+            return;
+        }
 
-            const meta = JSON.parse(metadata);
-            const $ = cheerio.load(fileData);
-            let newFile = fileData;
+        // Add script to HTML file if it exists
+        if (fs.existsSync(scriptBundle)) {
+            head.prepend("<script>").attr("src", "bundle.min.js", "type", "text/javascript");
+            newFile = $.html();
+        }
 
-            const head = $("head");
-            // define a function to add tags to head
-            const add = (type, _toAdd, def) => {
-                let elem = head.add(type);
-                const toAdd = _toAdd ? _toAdd : def;
-                for (const index in toAdd) {
-                    if (toAdd.hasOwnProperty(index)) {
-                        elem = elem.attr(index, toAdd[index]);
-                    }
+        if (fs.existsSync(metaFile)) {
+            fs.readFile(metaFile, function (err, metadata) {
+                if (err) {
+                    throw err;
                 }
-                newFile = elem.html();
-            };
-            // function to add links to <head>
-            const addLink = (toAdd, rel, href, size) => add("link", toAdd, {rel: rel, href: href, size: size});
-            // function to add meta to <head>
-            const addMeta = (toAdd, name, content) => add("link", toAdd, {name: name, content: content});
 
-            if (!meta.favicon) {
-                meta.favicon = {};
-            }
-            if (!meta.card) {
-                meta.card = {};
-            }
+                const meta = JSON.parse(metadata);
 
-            if (!meta.favicon.noFavicon) {
-                addLink(meta.favicon.apple, "apple-touch-icon", "/favicon/apple-touch-icon.png", "180x180");
-                addLink(meta.favicon.favlarge, "icon", "/favicon/favicon-32x32.png", "32x32");
-                addLink(meta.favicon.favsmall, "icon", "/favicon/favicon-16x16.png", "16x16");
-                addLink(meta.favicon.manifest, "manifest", "/favicon/manifest.json");
-                addLink(meta.favicon.ico, {rel: "shortcut icon", href: "/favicon/favicon.ico"});
-                addMeta(meta.favicon.ms, "msapplication-config", "/favicon/browserconfig.xml");
-                addMeta(meta.favicon.theme, "theme-color", "#ffffff");
-            }
+                // define a function to add tags to head
+                function add(type, _toAdd, def) {
+                    let elem = head.prepend("<" + type + ">");
+                    const toAdd = _toAdd ? _toAdd : def;
+                    for (const index in toAdd) {
+                        if (toAdd.hasOwnProperty(index)) {
+                            elem = elem.attr(index, toAdd[index]);
+                        }
+                    }
+                    newFile = $.html();
+                }
 
-            if (!meta.card.noCard) {
-                addMeta(meta.card.twitterCard, "twitter:card", "summary");
-                addMeta(meta.card.twitterUser, "twitter:site", "@Deanveloper");
-                addMeta(meta.card.ogType, "og:type", "website");
-                addMeta(meta.card.title, "og:title", "Dean Bassett's Website");
-                addMeta(meta.card.title, "twitter:title", "Dean Bassett's Website");
-                addMeta(meta.card.description, "og:description", "A place for my little creations.");
-                addMeta(meta.card.description, "twitter:description", "A place for my little creations.");
-                addMeta(meta.card.image, "og:image", "https://www.deanveloper.com/favicon/android-chrome-512x512.png");
-                addMeta(meta.card.image, "twitter:image", "https://www.deanveloper.com/favicon/android-chrome-512x512.png");
-                addMeta(meta.card.twitterImageAlt, "twitter:image:alt", "Dean Bassett's Website");
-            }
-        });
+                // function to add links to <head>
+                const addLink = (toAdd, rel, href, size) => add("link", toAdd, {rel: rel, href: href, size: size});
+                // function to add meta to <head>
+                const addMeta = (toAdd, name, content) => add("link", toAdd, {name: name, content: content});
+
+                if (!meta.favicon) {
+                    meta.favicon = {};
+                }
+                if (!meta.card) {
+                    meta.card = {};
+                }
+
+                if (!meta.favicon.noFavicon) {
+                    addLink(meta.favicon.apple, "apple-touch-icon", "/favicon/apple-touch-icon.png", "180x180");
+                    addLink(meta.favicon.favlarge, "icon", "/favicon/favicon-32x32.png", "32x32");
+                    addLink(meta.favicon.favsmall, "icon", "/favicon/favicon-16x16.png", "16x16");
+                    addLink(meta.favicon.manifest, "manifest", "/favicon/manifest.json");
+                    addLink(meta.favicon.ico, {rel: "shortcut icon", href: "/favicon/favicon.ico"});
+                    addMeta(meta.favicon.ms, "msapplication-config", "/favicon/browserconfig.xml");
+                    addMeta(meta.favicon.theme, "theme-color", "#ffffff");
+                }
+
+                if (!meta.card.noCard) {
+                    addMeta(meta.card.twitterCard, "twitter:card", "summary");
+                    addMeta(meta.card.twitterUser, "twitter:site", "@Deanveloper");
+                    addMeta(meta.card.ogType, "og:type", "website");
+                    addMeta(meta.card.title, "og:title", "Dean Bassett's Website");
+                    addMeta(meta.card.title, "twitter:title", "Dean Bassett's Website");
+                    addMeta(meta.card.description, "og:description", "A place for my little creations.");
+                    addMeta(meta.card.description, "twitter:description", "A place for my little creations.");
+                    addMeta(meta.card.image, "og:image", "https://www.deanveloper.com/favicon/android-chrome-512x512.png");
+                    addMeta(meta.card.image, "twitter:image", "https://www.deanveloper.com/favicon/android-chrome-512x512.png");
+                    addMeta(meta.card.twitterImageAlt, "twitter:image:alt", "Dean Bassett's Website");
+                }
+            });
+        }
     }
 });
 
